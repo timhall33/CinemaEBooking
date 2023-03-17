76e048845a3c72bc3f1d4116d531e27737502562
Matthew Gayle <matthewgayle@Matthews-MacBook-Pro.local>
2023-03-16 22:19:38 -0400

    merge

---
+++ frontend/src/FirebaseRegistration.js
@@ -0,0 +1,21 @@
+import { getAuth } from "firebase/auth";
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+import app from './Firebase'
+import email from './RegisterView'
+import password from './RegisterView'
+
+function register() {
+
+    const auth = getAuth(app);
+    console.log(email);
+   // if(validatePassword()) {
+      // Create a new user with email and password using firebase
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((res) => {
+            console.log(res.user)
+          })
+        .catch(err => console.log(err.message))
+    //}
+  }
+  export default register;
+  // ...
