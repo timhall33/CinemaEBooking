16462e6a22718c7502bc939229ca73646d05e6a1
Matthew Gayle <matthewgayle@Matthews-MacBook-Pro.local>
2023-04-25 20:33:13 -0400

    Booking confirmation email

--- Code/src/Controls/FirebaseBookingFunctions.js
+++ Code/src/Controls/FirebaseBookingFunctions.js
@@ -3,15 +3,18 @@ import {db} from './Firebase';
 import { bookingConverter } from '../Models/BookingModel';
 import { doc, setDoc, updateDoc, addDoc } from "firebase/firestore";
 import { Booking } from '../Models/BookingModel';
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, getDoc } from "firebase/firestore";
+import { auth } from '../Controls/Firebase';
+import {userConverter} from '../Models/UserModel';
+import  emailjs, { sendForm } from "@emailjs/browser"
 
 export async function storeBooking(movie, showTime, ticket,price, seat, address, payment, userId) {
 
     // adding document
     const ref = collection(db, "booking").withConverter(bookingConverter)
 
-    await addDoc(ref, new Booking(movie, showTime, ticket,price, seat, address, payment, userId))
-    .then((e) => {{
+    await addDoc(ref, new Booking(movie, showTime, ticket, price, seat, address, payment, userId))
+    .then(async (e) => {{
 
       console.log(e)
       const newRef = doc(db, "booking", e.id);
@@ -19,7 +22,15 @@ export async function storeBooking(movie, showTime, ticket,price, seat, address,
           bookingID: e.id
 
       })
- 
+      const ref = doc(db, "users", auth.currentUser.uid).withConverter(userConverter)
+      const docSnap = await getDoc(ref)
+      if (docSnap.exists()) {
+          const user = docSnap.data();
+          const name = user.firstName
+          const email = user.email
+          sendBookingConfirmation(name, email, movie, showTime.date, showTime.time, price);
+        }
+
      
   }})
   .catch((error) => {
@@ -44,4 +55,24 @@ export async function storeBooking(movie, showTime, ticket,price, seat, address,
             });
     });
     
+  }
+
+ async function sendBookingConfirmation(toName, email, movieTitle, date, time, orderTotal) {
+  const emailParams = {
+            email: email,
+            toName: toName,
+            movieTitle: movieTitle,
+            date: date,
+            time: time,
+            orderTotal: orderTotal
+
+        }
+        emailjs.send('service_zonv1b2', 'template_yn45lwh', emailParams,'Wu_JsPCsb84O3Td2K')
+        .then(response => {
+          console.log('Email sent successfully', response);
+        })
+        .catch(error => {
+          console.error('Failed to send email', error);
+        });
+
   }
